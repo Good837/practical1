5e29c40d910654c955b79d0f304a0d90f14085d7
Good837 <iruna.levchenko11@gmail.com>
2024-10-15 11:39:30 +0300

    «overload writing object to std::cout

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main() {
-    Car car;
+    Car car1;
     Car car1(1, "Toyota", 2020, 24000.0, "AB123CD", "1HGM82633A123456", 5, 4);
     Car car2(2, "Honda", 2021, 26000.0, "EF456GH", "1HGCM2633A654321", 5, 4);
 
     cout << "Car 1:\n";
-    car.display();
+    car1.display();
     cout << "Car 2:\n";
     car1.display();
     cout << "Car 3:\n";
