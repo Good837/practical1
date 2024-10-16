29f863d31468107a1b566f7dc40a6b61bdd1992f
Good837 <iruna.levchenko11@gmail.com>
2024-10-16 09:53:58 +0300

    do practical work №4

---
+++ base.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Redmi on 15.10.2024.
+//
+
+#include "base.h"
+
+namespace Base {
+} // Base
---
+++ base.h
@@ -0,0 +1,16 @@
+//
+// Created by Redmi on 15.10.2024.
+//
+
+#ifndef BASE_H
+#define BASE_H
+
+namespace Base {
+
+class base {
+
+};
+
+} // Base
+
+#endif //BASE_H
